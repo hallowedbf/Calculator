efd36f9efdcd535aa17ea155e85ae2a8c083a970
hallowedbf <79479077+hallowedbf@users.noreply.github.com>
2021-04-26 14:15:12 -0500

    added addition

--- Calculator/Form1.Designer.cs
+++ Calculator/Form1.Designer.cs
@@ -33,7 +33,7 @@
             this.exponentButton = new System.Windows.Forms.Button();
             this.sqrRootButton = new System.Windows.Forms.Button();
             this.divisionButton = new System.Windows.Forms.Button();
-            this.button77 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
             this.button8 = new System.Windows.Forms.Button();
             this.button9 = new System.Windows.Forms.Button();
             this.multiButton = new System.Windows.Forms.Button();
@@ -112,16 +112,17 @@
             this.divisionButton.Text = "➗";
             this.divisionButton.UseVisualStyleBackColor = true;
             // 
-            // button77
+            // button7
             // 
-            this.button77.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button77.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
-            this.button77.Location = new System.Drawing.Point(9, 218);
-            this.button77.Name = "button77";
-            this.button77.Size = new System.Drawing.Size(64, 64);
-            this.button77.TabIndex = 5;
-            this.button77.Text = "7";
-            this.button77.UseVisualStyleBackColor = true;
+            this.button7.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button7.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.button7.Location = new System.Drawing.Point(9, 218);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(64, 64);
+            this.button7.TabIndex = 5;
+            this.button7.Text = "7";
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
             // 
             // button8
             // 
@@ -133,6 +134,7 @@
             this.button8.TabIndex = 6;
             this.button8.Text = "8";
             this.button8.UseVisualStyleBackColor = true;
+            this.button8.Click += new System.EventHandler(this.button8_Click);
             // 
             // button9
             // 
@@ -144,6 +146,7 @@
             this.button9.TabIndex = 7;
             this.button9.Text = "9";
             this.button9.UseVisualStyleBackColor = true;
+            this.button9.Click += new System.EventHandler(this.button9_Click);
             // 
             // multiButton
             // 
@@ -166,6 +169,7 @@
             this.button4.TabIndex = 9;
             this.button4.Text = "4";
             this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
             // 
             // button5
             // 
@@ -177,6 +181,7 @@
             this.button5.TabIndex = 10;
             this.button5.Text = "5";
             this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
             // 
             // button6
             // 
@@ -188,6 +193,7 @@
             this.button6.TabIndex = 11;
             this.button6.Text = "6";
             this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
             // 
             // minusButton
             // 
@@ -210,6 +216,7 @@
             this.button1.TabIndex = 13;
             this.button1.Text = "1";
             this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // button2
             // 
@@ -221,6 +228,7 @@
             this.button2.TabIndex = 14;
             this.button2.Text = "2";
             this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
             // button3
             // 
@@ -232,6 +240,7 @@
             this.button3.TabIndex = 15;
             this.button3.Text = "3";
             this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
             // 
             // addButton
             // 
@@ -243,6 +252,7 @@
             this.addButton.TabIndex = 16;
             this.addButton.Text = "+";
             this.addButton.UseVisualStyleBackColor = true;
+            this.addButton.Click += new System.EventHandler(this.addButton_Click);
             // 
             // negativeButton
             // 
@@ -265,6 +275,7 @@
             this.button0.TabIndex = 18;
             this.button0.Text = "0";
             this.button0.UseVisualStyleBackColor = true;
+            this.button0.Click += new System.EventHandler(this.button0_Click);
             // 
             // decimalButton
             // 
@@ -287,6 +298,7 @@
             this.equalsButton.TabIndex = 20;
             this.equalsButton.Text = "=";
             this.equalsButton.UseVisualStyleBackColor = true;
+            this.equalsButton.Click += new System.EventHandler(this.equalsButton_Click);
             // 
             // backspaceButton
             // 
@@ -357,7 +369,7 @@
             this.Controls.Add(this.multiButton);
             this.Controls.Add(this.button9);
             this.Controls.Add(this.button8);
-            this.Controls.Add(this.button77);
+            this.Controls.Add(this.button7);
             this.Controls.Add(this.divisionButton);
             this.Controls.Add(this.sqrRootButton);
             this.Controls.Add(this.exponentButton);
@@ -377,7 +389,7 @@
         private System.Windows.Forms.Button exponentButton;
         private System.Windows.Forms.Button sqrRootButton;
         private System.Windows.Forms.Button divisionButton;
-        private System.Windows.Forms.Button button77;
+        private System.Windows.Forms.Button button7;
         private System.Windows.Forms.Button button8;
         private System.Windows.Forms.Button button9;
         private System.Windows.Forms.Button multiButton;
